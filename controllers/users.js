a0a45c74c6f4351bdb3a04e5169caa56572c4bbc
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-10-08 18:46:43 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,12 @@
 const User = require('../models/user');
 
-/* module.exports.getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
-}; */
-
 module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((user) => res.send(user))
     .catch(next);
 };
 
-module.exports.getUserById = (req, res) => {
+/* module.exports.getUserById = (req, res) => {
   if (req.params.userId.length === 24) {
     User.findById(req.params.userId)
       .then((user) => {
@@ -26,6 +20,21 @@ module.exports.getUserById = (req, res) => {
   } else {
     res.status(400).send({ message: 'Неверный _id' });
   }
+}; */
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .orFail(() => res.status(404).send({ message: 'Пользователь не найден' }))
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Неверный _id' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 
 module.exports.addUser = (req, res) => {
