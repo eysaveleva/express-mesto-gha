df44840cf4bc7bb2ef3d5f4c8ffed77dd91c31f4
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-10-13 16:50:15 +0300

    Delete routes/signin.js

--- routes/signin.js
+++
@@ -1,7 +0,0 @@
-const signinRouter = require('express').Router();
-const { validateLogin } = require('../middlewares/validation');
-const { login } = require('../controllers/users');
-
-signinRouter.post('/signin', validateLogin, login);
-
-module.exports = { signinRouter };
