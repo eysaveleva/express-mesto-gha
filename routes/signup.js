d615ed636722d3a5dd30efe66adb0340c74de87b
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-10-13 16:50:23 +0300

    Delete routes/signup.js

--- routes/signup.js
+++
@@ -1,7 +0,0 @@
-const signupRouter = require('express').Router();
-const { validateAddUser } = require('../middlewares/validation');
-const { addUser } = require('../controllers/users');
-
-signupRouter.post('/signup', validateAddUser, addUser);
-
-module.exports = { signupRouter };
